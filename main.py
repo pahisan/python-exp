3ea198b879294f90cd3ec3cb40db6bf1170ab72a
pahisan <82726639+pahisan@users.noreply.github.com>
2021-05-16 17:01:23 -0400

    i added, subtracted, divided and multiplyed

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ import datetime
 def timeofCity ():
     return datetime.datetime.now()
 print(timeofCity())
+
+
+a = 1
+b = 1
+print(a + b)
+
+w = 1
+x = 2
+print(w * x)
+
+c = 3
+g = 3
+print(c/g)
+
+y = 5
+z = 5
+print(y - z)
